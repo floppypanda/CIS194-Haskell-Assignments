2459bf0ba075adf539f942f6e80b5250aa08fda0
floppypanda <floppypanda@users.noreply.github.com>
2018-10-21 17:13:43 -0600

    Adding function for finding local maxima in integer list.

--- university-pennsylvania/assignment-3/Golf.hs
+++ university-pennsylvania/assignment-3/Golf.hs
@@ -11,3 +11,11 @@ skip (elem:otherElems) position numSkip
 skips :: [a] -> [[a]]
 skips [] = []
 skips list = map (skip list 1) [1..(length list)]
+
+localMaxima :: [Int] -> [Int]
+localMaxima [] = []
+localMaxima (x1:[]) = []
+localMaxima (x1:x2:[]) = []
+localMaxima (x1:x2:x3:xs)
+  | x1 < x2 && x2 > x3 = x2:(localMaxima (x2:x3:xs))
+  | otherwise = localMaxima (x2:x3:xs)
