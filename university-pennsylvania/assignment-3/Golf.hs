ab9328a90e4a872b4560af16a344b39275d0c1cf
floppypanda <floppypanda@users.noreply.github.com>
2018-10-22 16:54:05 -0600

    Adding functions for constructing histogram display.

--- university-pennsylvania/assignment-3/Golf.hs
+++ university-pennsylvania/assignment-3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 -- Pass in list, starting position, and number to skip.
 skip :: [a] -> Int -> Int -> [a]
 skip [] _ _ = []
@@ -19,3 +21,17 @@ localMaxima (x1:x2:[]) = []
 localMaxima (x1:x2:x3:xs)
   | x1 < x2 && x2 > x3 = x2:(localMaxima (x2:x3:xs))
   | otherwise = localMaxima (x2:x3:xs)
+
+needAsterix :: Int -> Int -> String
+needAsterix level num
+    | num >= level = "*"
+    | otherwise = " "
+
+buildHistogramLine :: [Int] -> Int -> String
+buildHistogramLine [] _ = ""
+buildHistogramLine frequencies level = foldr (++) "" (map (needAsterix level) frequencies)
+
+buildHistogram :: [Int] -> String
+buildHistogram frequencies = do
+    let maxFrequency = maximum frequencies
+    intercalate "\n" (map (buildHistogramLine frequencies) [maxFrequency,(maxFrequency-1)..1]) ++ "\n"
