8299c0584dda7cf25b67d19aa8df92c7a634be2c
floppypanda <floppypanda@users.noreply.github.com>
2018-10-29 19:58:55 -0600

    Adding code for evaluating expression strings.

--- university-pennsylvania/assignment-5/Calc.hs
+++ university-pennsylvania/assignment-5/Calc.hs
@@ -1,8 +1,18 @@
 module Calc where
 
+import Data.Maybe
+
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit num) = num
 eval (Add expOne expTwo) = (eval expOne) + (eval expTwo)
 eval (Mul expOne expTwo) = (eval expOne) * (eval expTwo)
+
+evalStr :: String -> Maybe Integer
+evalStr str = do
+    let parseResult = parseExp Lit Add Mul str
+    case parseResult of
+        Nothing -> Nothing
+        Just res -> Just (eval res)
